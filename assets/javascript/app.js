2cd5bc64b02265848b97a43c865ef81227f0fa76
Cmack704 <craigmack704@gmail.com>
2019-07-06 20:00:15 -0400

    made global variables and arrays

---
+++ assets/javascript/app.js
@@ -0,0 +1,26 @@
+//------------------------
+
+// global variables
+var correctAns = 0;
+var wrongAns = 0;
+var unanswered = 0;
+
+//arrays
+var questions = ["Who is the all-time leader of the NBA in points scored, with 38,387 points?", 
+    "Which quarteback has the most rushing touchdowns in NFL history with 58 touchdowns?",
+    "Which athlete is the only one to appear in a Super Bowl and a World Series?", 
+    "How many touchdowns did Tom Brady throw in 2007, when he broke the record for touchdowns thrown in a single season?", 
+    "Who was the quarteback that later broke Tom Brady's single season touchdown record?",
+    "Which pitcher is the all-time leader in strikeouts in MLB history?",
+    "Which NBA player was the first to average a triple double in the NBA finals?", 
+    "Who is the only player to win NBA finals MVP on the losing team?"
+]
+var correctAnswers = ["Kareem Abdul-Jabbar", "Cam Newton", "Deion Sanders", "50", "Peyton Manning", "Nolan Ryan", "Lebron James", "Jerry West"]
+var wrongAnswers = ["Michael Jordan", "LeBron James", "Wilt Chamberlin",
+    "Michael Vick", "Randall Cunningham", "Steve Young",
+    "Bo Jackson", "Russel Wilson",
+    "55", "48", "60",
+    "Drew Brees", "Joe Montana", "Patrick MaHomes",
+    "Randy Johnson", "Roger Clemens", "Cy Young",
+    "Magic Johnson", "Larry Bird", "Karl Malone"
+]
