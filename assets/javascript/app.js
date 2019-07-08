6160fce5df5c3c0cb6b28e168532eb3371f56e49
Cmack704 <craigmack704@gmail.com>
2019-07-08 18:21:25 -0400

    edited html and js

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -4,6 +4,8 @@
 var correctAns = 0;
 var wrongAns = 0;
 var unanswered = 0;
+
+
 var questions = [
     { q: "Who is the all-time leader of the NBA in points scored, with 38,387 points?",
     answers: ["Michael Jordan", "Kevin Durant", "Kareem Abdul-Jabbar", "Wilt Chamberlin"],
@@ -30,7 +32,7 @@ var questions = [
     correct: 1
     },
     { q: "Which NBA player was the first to average a triple double in the NBA finals?",
-    answers: ["Magic Johnson", "Lebron James", "Larry Bird", "Russell Westbrook"],
+    answers: ["Magic Johnson", "LeBron James", "Larry Bird", "Russell Westbrook"],
     correct: 1
     },
     { q: "Who is the only player to win NBA finals MVP on the losing team?",
@@ -38,22 +40,24 @@ var questions = [
     correct: 0
     },
 ]
-//arrays
-var questionsText = ["Who is the all-time leader of the NBA in points scored, with 38,387 points?", 
-    "Which quarteback has the most rushing touchdowns in NFL history with 58 touchdowns?",
-    "Which athlete is the only one to appear in a Super Bowl and a World Series?", 
-    "How many touchdowns did Tom Brady throw in 2007, when he broke the record for touchdowns thrown in a single season?", 
-    "Who was the quarteback that later broke Tom Brady's single season touchdown record?",
-    "Which pitcher is the all-time leader in strikeouts in MLB history?",
-    "Which NBA player was the first to average a triple double in the NBA finals?", 
-    "Who is the only player to win NBA finals MVP on the losing team?"
-]
+
+
 
 
 $(document).ready(function(){
+
+    $(".col-sml").hide();
+    $(".question").hide();
+
+
+
+
     $(".btn").click(function(){
         $("img").hide();
-        $("button").text("Submit")
+        $(".col-sml").show();
+        $(".question").show();
+
+        $(".btn").text("Submit")
 
         var mins = 2;
         var secs = mins * 60;
@@ -67,35 +71,13 @@ $(document).ready(function(){
         }
         
 
-
-        $("#quest-1").text(questionsText[0]);
-        $("#quest-2").text(questionsText[1]);
-        $("#quest-3").text(questionsText[2]);
-        $("#quest-4").text(questionsText[3]);
-        $("#quest-5").text(questionsText[4]);
-        $("#quest-6").text(questionsText[5]);
-        $("#quest-7").text(questionsText[6]);
-        $("#quest-8").text(questionsText[7]);
-
-       var newbutton1 = $("<button class='col-sml-3 answer'></button>");
-       var newbutton2 = $("<button class='col-sml-3 answer'></button>");
-       var newbutton3 = $("<button class='col-sml-3 answer'></button>");
-       var newbutton4 = $("<button class='col-sml-3 answer'></button>");
-
-       $(".answers").append(newbutton1, newbutton2, newbutton3, newbutton4);
-
-
-
-
-
-
-
+       
 
 
         
         $(".btn").click(function(){
             $(".question").hide();
-            $(".answer").hide();
+            $(".col-sml").hide();
             $(".answers").hide();
             $(".btn").hide();
 
