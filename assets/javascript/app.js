2e6f4516e1b99202b4a3e61503e5f1baee19bdbc
Craig McAlister <craigmack704@gmail.com>
2019-07-08 12:58:40 -0400

    made objects for questionsand answers

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -4,9 +4,42 @@
 var correctAns = 0;
 var wrongAns = 0;
 var unanswered = 0;
-
+var questions = [
+    { q: "Who is the all-time leader of the NBA in points scored, with 38,387 points?",
+    answers: ["Michael Jordan", "Kevin Durant", "Kareem Abdul-Jabbar", "Wilt Chamberlin"],
+    correct: 2
+    },
+    { q: "Which quarteback has the most rushing touchdowns in NFL history with 58 touchdowns?",
+    answers: ["Michael Vick", "Cam Newton", "Randall Cunningham", "Steve Young"],
+    correct: 1
+    },
+    { q: "Which athlete is the only one to appear in a Super Bowl and a World Series?",
+    answers: ["Bo Jackson", "Deion Sanders", "Russel Wilson", "Jim Brown"],
+    correct: 1
+    },
+    { q: "How many touchdowns did Tom Brady throw in 2007, when he broke the record for touchdowns thrown in a single season?",
+    answers: ["55", "48", "50", "60"],
+    correct: 2
+    },
+    { q: "Who was the quarteback that later broke Tom Brady's single season touchdown record?",
+    answers: ["Peyton Manning", "Drew Brees", "Joe Montana", "Patrick MaHomes"],
+    correct: 0
+    },
+    { q: "Which pitcher is the all-time leader in strikeouts in MLB history?",
+    answers: ["Randy Johnson", "Nolan Ryan", "Roger Clemens", "Cy Young"],
+    correct: 1
+    },
+    { q: "Which NBA player was the first to average a triple double in the NBA finals?",
+    answers: ["Magic Johnson", "Lebron James", "Larry Bird", "Russell Westbrook"],
+    correct: 1
+    },
+    { q: "Who is the only player to win NBA finals MVP on the losing team?",
+    answers: ["Jerry West", "Karl Malone", "Kobe Bryant", "Bill Russell"],
+    correct: 0
+    },
+]
 //arrays
-var questions = ["Who is the all-time leader of the NBA in points scored, with 38,387 points?", 
+var questionsText = ["Who is the all-time leader of the NBA in points scored, with 38,387 points?", 
     "Which quarteback has the most rushing touchdowns in NFL history with 58 touchdowns?",
     "Which athlete is the only one to appear in a Super Bowl and a World Series?", 
     "How many touchdowns did Tom Brady throw in 2007, when he broke the record for touchdowns thrown in a single season?", 
@@ -15,15 +48,7 @@ var questions = ["Who is the all-time leader of the NBA in points scored, with 3
     "Which NBA player was the first to average a triple double in the NBA finals?", 
     "Who is the only player to win NBA finals MVP on the losing team?"
 ]
-var answers = [ "Michael Jordan", "Kevin Durant", "Kareem Abdul-Jabbar", "Wilt Chamberlin",
-    "Michael Vick", "Cam Newton", "Randall Cunningham", "Steve Young",
-    "Bo Jackson", "Deion Sanders", "Russel Wilson",
-    "55", "48", "50", "60",
-    "Peyton Manning", "Drew Brees", "Joe Montana", "Patrick MaHomes",
-    "Randy Johnson", "Nolan Ryan", "Roger Clemens", "Cy Young",
-    "Magic Johnson", "Lebron James", "Larry Bird", 
-    "Jerry West", "Karl Malone"
-]
+
 
 $(document).ready(function(){
     $(".btn").click(function(){
@@ -43,14 +68,14 @@ $(document).ready(function(){
         
 
 
-        $("#quest-1").text(questions[0]);
-        $("#quest-2").text(questions[1]);
-        $("#quest-3").text(questions[2]);
-        $("#quest-4").text(questions[3]);
-        $("#quest-5").text(questions[4]);
-        $("#quest-6").text(questions[5]);
-        $("#quest-7").text(questions[6]);
-        $("#quest-8").text(questions[7]);
+        $("#quest-1").text(questionsText[0]);
+        $("#quest-2").text(questionsText[1]);
+        $("#quest-3").text(questionsText[2]);
+        $("#quest-4").text(questionsText[3]);
+        $("#quest-5").text(questionsText[4]);
+        $("#quest-6").text(questionsText[5]);
+        $("#quest-7").text(questionsText[6]);
+        $("#quest-8").text(questionsText[7]);
 
        var newbutton1 = $("<button class='col-sml-3 answer'></button>");
        var newbutton2 = $("<button class='col-sml-3 answer'></button>");
